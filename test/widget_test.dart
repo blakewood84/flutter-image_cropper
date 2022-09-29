fa45dd468f92002ff6bfac9beb7a24a01b5bae5c
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-09-29 10:25:27 -0700

    implement image cropper

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:image_cropper/main.dart';
+import 'package:image_cropper_test/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
